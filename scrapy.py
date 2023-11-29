4ddb6bf31487e24312bdc8dcf751f2970560f246
eclaflin <eric.claflin@gmail.com>
2023-11-28 20:44:28 -0500

    added some notes

--- scrapy.py
+++ scrapy.py
@@ -14,15 +14,20 @@ url = 'https://www.zillow.com/homedetails/15-Maine-Ter-A-Somerville-MA-02145/205
 
 res = requests.get(headers=headers, url=url)
 
+# what is an object of the BeautifulSoup class?  what can i do with that to make json.loads() easier?
 content = BeautifulSoup(res.text, 'html.parser').find("script", {"id": "__NEXT_DATA__"})
 
 pre_data = json.loads(content.text)
-
+# how do I more efficientyly parse a json structure
 data = json.loads(pre_data['props']['pageProps']['componentProps']['gdpClientCache'])
 
+# data is a variable containing the output of json .loads 
+# so what is that that I am accessing with .keys(), is it a dictionary?
 keys_view = data.keys()
 
+# if data.keys() is a list, why do I need to cast keys_view to a list?  isn't it already a list?
 first_key = list(keys_view)[0]
 
+# first key is a dictionary that contains all of the data that I want 
 print(data[first_key]['property'])
 
