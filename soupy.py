580f5b563fa00505ee7fd47c7872ded5feffcf66
eclaflin <eric.claflin@gmail.com>
2023-12-04 19:49:46 -0500

    completed class based implementation of scrape

--- soupy.py
+++ soupy.py
@@ -1,51 +1,116 @@
 import json
 import requests
 import re 
-from bs4 import BeautifulSoup
 import sys
+from bs4 import BeautifulSoup
+from typing import Dict
 
-headers = {
-    'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
-    'accept-encoding': 'gzip, deflate, br, zstd',
-    'accept-language': 'en-US,en;q=0.9',
-    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-}
+#headers = {
+#    'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+#    'accept-encoding': 'gzip, deflate, br, zstd',
+#    'accept-language': 'en-US,en;q=0.9',
+#    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+#}
 
-url = 'https://www.zillow.com/homedetails/15-Maine-Ter-A-Somerville-MA-02145/2054344316_zpid/'
+#url = 'https://www.zillow.com/homedetails/15-Maine-Ter-A-Somerville-MA-02145/2054344316_zpid/'
 
-res = requests.get(headers=headers, url=url)
+def scrape_page(url, headers):
 
-soup = BeautifulSoup(res.text, 'html.parser')
+    res = requests.get(url, headers=headers)
 
-#tag = soup.script
-#print(tag.string)
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-script_tag = soup.find(attrs={"id":"__NEXT_DATA__","type":"application/json"})
+    script_tag = soup.find(attrs={"id":"__NEXT_DATA__","type":"application/json"})
 
-# check if the soup.find successfully found a <script> tag
-if script_tag:
-    
-    # extract content of script string
-    script_content = script_tag.string
-
-    # load content to json object
-    data_dict = json.loads(script_content)
-
-    # extract desired data:
-    try:
-
-        page_data = json.loads(data_dict['props']['pageProps']['componentProps']['gdpClientCache'])
-        listing_data = page_data[list(page_data.keys())[0]]['property']
- 
-        print(listing_data.keys())
-
-    except KeyError as e:
-        print(f"Error accessing nested keys:{e}")
-
-else:
-    print("Script tag not found")
-
-
+    # check if the soup.find successfully found a <script> tag
+    if script_tag:
         
-    
+        # extract content of script string
+        script_content = script_tag.string
 
+        # load content to json object
+        data_dict = json.loads(script_content)
+
+        # extract desired data:
+        try:
+
+            page_data = json.loads(data_dict['props']['pageProps']['componentProps']['gdpClientCache'])
+            listing_data = page_data[list(page_data.keys())[0]]['property']
+    
+            return listing_data
+
+        except KeyError as e:
+            print(f"Error accessing nested keys:{e}")
+
+    else:
+        print("Script tag not found")
+
+class PropertyListing:
+    
+    def __init__(self, 
+                 zpid, 
+                 city, 
+                 state, 
+                 homeStatus, 
+                 address, 
+                 bedrooms, 
+                 bathrooms, 
+                 price, 
+                 yearBuilt, 
+                 streetAddress, 
+                 zipcode, 
+                 homeType, 
+                 monthlyHoaFee,
+                 livingArea,
+                 taxHistory, 
+                 priceHistory, 
+                 timeOnZillow, 
+                 pageViewCount, 
+                 favoriteCount,
+                 daysOnZillow, 
+                 latitude, 
+                 longitude,
+                 propertyTaxRate,
+                 lotSize,
+                 annualHomeownersInsurance):
+        
+        self.zpid = zpid 
+        self.city = city 
+        self.state = state 
+        self.homeStatus = homeStatus 
+        self.address = address 
+        self.bedrooms = bedrooms 
+        self.bathrooms = bathrooms 
+        self.price = price 
+        self.yearBuilt = yearBuilt 
+        self.streetAddress = streetAddress 
+        self.zipcode = zipcode 
+        self.homeType = homeType 
+        self.monthlyHoaFee = monthlyHoaFee
+        self.livingArea = livingArea
+        self.taxHistory = taxHistory 
+        self.priceHistory = priceHistory 
+        self.timeOnZillow = timeOnZillow 
+        self.pageViewCount = pageViewCount 
+        self.favoriteCount = favoriteCount
+        self.daysOnZillow = daysOnZillow 
+        self.latitude = latitude 
+        self.longitude = longitude
+        self.propertyTaxRate = propertyTaxRate
+        self.lotSize = lotSize
+        self.annualHomeownersInsurance = annualHomeownersInsurance
+
+
+    @classmethod
+    def from_dict(cls, listing_dict: Dict[str, any]) -> 'PropertyListing':
+        # Define a set of allowed keys
+        allowed_keys = {'zpid', 'city', 'state', 'homeStatus', 'address', 'bedrooms', 'bathrooms',
+                        'price', 'yearBuilt', 'streetAddress', 'zipcode', 'homeType', 'monthlyHoaFee',
+                        'livingArea', 'taxHistory', 'priceHistory', 'timeOnZillow', 'pageViewCount',
+                        'favoriteCount', 'daysOnZillow', 'latitude', 'longitude', 'propertyTaxRate',
+                        'lotSize', 'annualHomeownersInsurance'}
+
+        # Extract only the allowed keys from listing_dict
+        filtered_dict = {k: v for k, v in listing_dict.items() if k in allowed_keys}
+
+        return cls(**filtered_dict)
