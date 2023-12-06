c0a98e0ae6b5b49e65c79a01668d061ecbdfb21f
eclaflin <eric.claflin@gmail.com>
2023-12-05 19:06:28 -0500

    finished itemized calc of mortgage + tax

--- quote.py
+++ quote.py
@@ -7,10 +7,22 @@ class PropertyQuote:
         self.financed_amt = (self.property_listing.price - self.down_payment)
         self.mortgage_term_years = mortgage_term_years
 
+    def calc_tax_pmt(self):
+        try:
+            self.curr_tax_ass = self.property_listing.taxHistory[0]['taxPaid']
+            self.monthly_tax_pmt = self.curr_tax_ass/12
+
+        except:
+            raise valueError('No tax history available on zillow') 
+        
+        pass
+
     def calc_mortgage_pmt(self):
+        self.calc_tax_pmt()
+
         self.monthly_interest_rate = self.interest_rate/12
         self.lifetime_pmts = self.mortgage_term_years * 12
-        self.monthly_pmt = (
+        self.monthly_loan_pmt = (
             self.financed_amt*(
                 (self.monthly_interest_rate*(1+self.monthly_interest_rate)**self.lifetime_pmts)
                 /
@@ -20,9 +32,11 @@ class PropertyQuote:
 
         self.mothly_principal_pmt = self.financed_amt/(self.lifetime_pmts)
 
-        self.monthly_interest_pmt = self.monthly_pmt - self.mothly_principal_pmt 
+        self.monthly_interest_pmt = self.monthly_loan_pmt - self.mothly_principal_pmt 
 
-        fmt_output = "total monthly payment = %d\n monthly principal = %d\n monthly interest = %d\n" % (
-            self.monthly_pmt, self.mothly_principal_pmt, self.monthly_interest_pmt)
+        self.total_monthly_pmt = self.monthly_loan_pmt + self.monthly_tax_pmt
+
+        fmt_output = "total monthly payment = %d\n monthly principal = %d\n monthly interest = %d\n monthly_tax %d" % (
+            self.total_monthly_pmt, self.mothly_principal_pmt, self.monthly_interest_pmt, self.monthly_tax_pmt)
         
         print(fmt_output)
