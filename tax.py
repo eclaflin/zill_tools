57c0b16e96387078af18d650140110d4bfee0b11
eclaflin <eric.claflin@gmail.com>
2023-12-18 20:43:58 -0500

    completed ma rate pull

--- tax.py
+++ tax.py
@@ -5,5 +5,11 @@ csv_filepath = os.path.join("tax_rates","ma_tax_rates.csv")
 
 df_tax_rates = pd.read_csv(csv_filepath)
 
-print(df_tax_rates.head())
+#print(df_tax_rates.head())
 
+def rate_pull(town):
+    df_out = df_tax_rates.loc[
+        (df_tax_rates['Municipality']==town) & (df_tax_rates['Fiscal Year']==2023)
+        ]
+    
+    return df_out['Residential'].iloc[0].astype('float')
