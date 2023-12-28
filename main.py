dae5df84ff6c370462e5534c1bd355eb3df2ab9c
eclaflin <eric.claflin@gmail.com>
2023-12-28 13:47:55 -0500

    moved quote function back to quote module

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-from listing import scrape_page, PropertyListing 
-from quote import PropertyQuote
-from rates import get_latest_rate
 from simple_term_menu import TerminalMenu
-
-def quote_a_property():
-
-    url = input("Enter a Zillow Listing URL: ")
-    data_dict = scrape_page(url)
-    listing = PropertyListing.from_dict(data_dict)
-
-    print("The list price is %d\n" % listing.price)
-    down = input("Enter a down payment amount: ")
-    int_rate = float(get_latest_rate())/100
-
-    quote = PropertyQuote(property_listing=listing, interest_rate=float(int_rate), down_payment=int(down), mortgage_term_years=30)
-
-    quote.calc_mortgage_pmt()
-    quote.write_quote()
+from quote import quote_a_property
 
 def closing_cost_estimator():
     print('this is the closing cost estimatory')
--- quote.py
+++ quote.py
@@ -1,4 +1,6 @@
 from prettytable import PrettyTable
+from listing import scrape_page, PropertyListing 
+from rates import get_latest_rate
 from tax import rate_pull
 
 class PropertyQuote:
@@ -79,3 +81,18 @@ class PropertyQuote:
         #    self.total_monthly_pmt, self.mothly_principal_pmt, self.monthly_interest_pmt, self.monthly_tax_pmt)
         
         #print(fmt_output)
+
+def quote_a_property():
+
+    url = input("Enter a Zillow Listing URL: ")
+    data_dict = scrape_page(url)
+    listing = PropertyListing.from_dict(data_dict)
+
+    print("The list price is %d\n" % listing.price)
+    down = input("Enter a down payment amount: ")
+    int_rate = float(get_latest_rate())/100
+
+    quote = PropertyQuote(property_listing=listing, interest_rate=float(int_rate), down_payment=int(down), mortgage_term_years=30)
+
+    quote.calc_mortgage_pmt()
+    quote.write_quote()
